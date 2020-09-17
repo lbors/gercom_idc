80cf8d56635eaba2f16d11dccfa64a105d25df2b
root <lucas_borges95@yahoo.com.br>
2020-09-17 17:32:50 +0000

    Correções

--- dcs/idc/dc-slice-controller/Code/run.py
+++
@@ -1,6 +0,0 @@
-from vm_factory import connection
-connection.test_connection()
-
-
-from rest import server
-# from network_manager import network_manager
--- dcs/idc/wan-slice-controller-agent/src/settings.py
+++
@@ -1,9 +0,0 @@
-OVSDB_ADDR = 'tcp:10.126.1.187:6640'
-LOCAL_IP = "10.126.1.187"
-
-# IP externo que vai ser mapeado para IP interno
-REMOTE_ADDRESS_MAPPING = {
-#  [server_public_ip : xen_host]
-    "10.126.1.187" : "10.126.1.187"
-}
-

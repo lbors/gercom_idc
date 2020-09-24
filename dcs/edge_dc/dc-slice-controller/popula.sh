910080e50f824e5f29cefdf5dba8ea325015d942
necos <lucas_borges95@yahoo.com.br>
2020-09-24 15:14:43 +0000

    reformulação

---
+++ dcs/edge_dc/dc-slice-controller/popula.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+MY_IP="10.126.1.187"
+
+curl -X POST --data-binary @Yaml/POST/post_user.yaml -H "Content-type: text/x-yaml" http://"$MY_IP":5000/user
+
+curl -X POST --data-binary @Yaml/POST/post_controller.yaml -H "Content-type: text/x-yaml" http://"$MY_IP":5000/controller
+
+curl -X POST --data-binary @Yaml/POST/post_vim_type.yaml -H "Content-type: text/x-yaml" http://"$MY_IP":5000/vim_type
+
+curl -X POST --data-binary @Yaml/POST/post_template.yaml -H "Content-type: text/x-yaml" http://"$MY_IP":5000/template
+
+curl -X POST --data-binary @Yaml/POST/post_host.yaml -H "Content-type: text/x-yaml" http://"$MY_IP":5000/host
+
